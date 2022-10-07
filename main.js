23f675d706aff64995e204ca7b7610a22c54acc6
Zhou Sun <me@Zhou-Sun.localdomain>
2022-10-07 21:25:15 +0100

    optimize code and add write files features

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import { permutation } from './permutation.js';
+import { permutation } from './modules/permutation.js'
+import { append } from './modules/file.js'
 
-const events = ['a', 'b', 'c', 'd', 'e'];
+const events = ['a', 'b', 'c', 'd', 'e']
+const result = permutation(events, 3)
+const file = 'test.txt'
 
-const result = permutation(events, 3);
+result.forEach((element) => {
+  append(file, element)
+})
 
-console.log(result);
+console.log(result)
---
+++ modules/permutation.js
@@ -0,0 +1,25 @@
+export function permutation(events, trialCount) {
+  const result = []
+
+  if (trialCount === 1) return events
+  if (events.length < trialCount) console.error('invalid input')
+
+  rec([], events, trialCount, result)
+
+  return result
+}
+
+function rec(input, events, n, output) {
+  if (input.length === n) {
+    return output.push(`['${input.join(`' ,'`)}']`)
+  }
+
+  for (let i = 0; i < events.length; i++) {
+    const cache = events.splice(i, 1).join()
+    const temp = [...input]
+
+    temp.push(cache)
+    rec(temp, events, n, output)
+    events.splice(i, 0, cache)
+  }
+}
